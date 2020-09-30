5a6b754235aa17fdb8ab60df22ae04c892414f39
Edge <44886569+edgeeepangan@users.noreply.github.com>
2020-10-01 02:28:13 +0800

    Add files via upload

---
+++ Avery/app/src/main/java/com/example/avery/AddOth.kt
@@ -0,0 +1,8 @@
+package com.example.avery
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AddOth : AppCompatActivity(){
+    ActionBar actionBar = getActionBar()
+    actionBar.setDisplayHomeAsUpEnabled(true)
+}
